d3df0ad396bebefd89d4e463767288d8717c79bf
muyu <15595060891@163.com>
2025-06-14 00:56:57 +0800

    同步修改

--- src/views/PageView/pageView.ts
+++ src/views/PageView/pageView.ts
@@ -8,8 +8,8 @@ export function recordReadingTime(bookId: number) {
     // 开始时间
     let start = Date.now() / 1000;
 
-    // 小于 60 秒不记录
-    const minTime = 60;
+    // 小于 10 秒不记录
+    const minTime = 10;
 
     let cost = 0;
 
@@ -27,23 +27,35 @@ export function recordReadingTime(bookId: number) {
     // 当窗口获得焦点时触发
     window.addEventListener('focus', focusFunc);
 
-    onBeforeUnmount(() => {
-
-        window.removeEventListener('blur', blurFunc);
-        window.removeEventListener('focus', focusFunc);
-
-        cost += Date.now() / 1000 - start;
+    const updateCostFunc = () => {
+        const now = Date.now() / 1000;
+        cost += now - start;
+        start = now;
         cost = Math.floor(cost);
-        if (cost <= minTime) {
+
+        if (cost < minTime) {
             return;
         }
 
         console.log(`-------- cost = ${cost} --------`);
 
         addReadingCostApi(bookId, cost)
+            .then(() => {
+                cost = 0;
+            })
             .catch(err => {
                 console.log(err);
             })
+    }
+
+    const intervalId = setInterval(updateCostFunc, 10 * 1000);
+
+
+    onBeforeUnmount(() => {
+        clearInterval(intervalId);
+        window.removeEventListener('blur', blurFunc);
+        window.removeEventListener('focus', focusFunc);
+        updateCostFunc();
     });
 
 }
